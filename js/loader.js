8d5da4f955d73e4d0cdd66519dc6eb0413067530
ali <aloush.aam@gmail.com>
2021-01-02 11:43:00 +0200

    smooth-scroll and some animations

--- js/loader.js
+++ js/loader.js
@@ -1,11 +1,14 @@
 $(document).ready(()=>{
-    $('.sidenav').sidenav({
-  
+    var scroll = new SmoothScroll('a[href*="#"]', {
+        speed: 1000,
+        easing:'easeInQuad',
+        topOnEmptyHash: true,
     });
+    $('.sidenav').sidenav();
     $('.fixed-action-btn').floatingActionButton();
     $('.tabs').tabs();
     $('.materialboxed').materialbox();
     $('.slider').slider();
     $('.carousel').carousel();
-
 });
+
